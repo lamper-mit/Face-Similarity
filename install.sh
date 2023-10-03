2e982693c1fea5a72947729421f21209d81a731c
Mitchell Lamper <67124530+lamper-mit@users.noreply.github.com>
2023-10-03 14:56:37 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Step 1: Create a virtual environment named 'venv'
+python3 -m venv venv
+
+# Step 2: Activate the virtual environment
+source venv/bin/activate
+
+# Step 3: Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Step 4: Make the main script (compare.py) executable
+chmod +x compare.py
+
+echo "Setup completed. Virtual environment created and dependencies installed."
+echo "To activate the virtual environment, use: source venv/bin/activate"
