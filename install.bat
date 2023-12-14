30d260b3b2310f544341fdb19163d1714a071942
Mitchell Lamper <67124530+lamper-mit@users.noreply.github.com>
2023-12-13 16:20:14 -0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ REM Step 2: Activate the virtual environment
 call venv\Scripts\activate
 
 REM Step 3: Install dependencies from requirements.txt
-pip install -r requirements.txt
+pip install -r requirements_windows.txt
 
 echo Setup completed. Virtual environment created and dependencies installed.
 echo To activate the virtual environment, use: .\venv\Scripts\activate
