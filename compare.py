76fc9be6d68a95ffb5bd5a620dbb781bd356203c
Mitchell Lamper <67124530+lamper-mit@users.noreply.github.com>
2024-02-21 16:13:07 -0500

    fix windows variable not initializing

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+0000000000000000000000000#!/usr/bin/env python
 import sys
 import os
 import shutil
@@ -19,12 +19,13 @@ if os.name == 'nt':
 
 if not windows:
     import pyheif
+    windows = False
 
     def convert_heic_to_jpg(heic_path):
         heif_file = pyheif.read(heic_path)
         image = Image.frombytes(
             heif_file.mode, 
-            heif_file.size, 
+            heif_file.size,
             heif_file.data,
             "raw",
             heif_file.mode,
